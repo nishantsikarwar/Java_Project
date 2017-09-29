03044793a065ebe05adcfb84ab222c98492c3a49
nishantsikarwar <nishant.sikarwar007@gmail.com>
2017-09-29 19:22:33 +0530

    added_Hash_Maps

--- lib/src/main/java/com/example/Java_Application.java
+++ lib/src/main/java/com/example/Java_Application.java
@@ -2,11 +2,13 @@ package com.example;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.math.BigDecimal;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.Date;
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.ListIterator;
+import java.util.Set;
 
 
 public class Java_Application {
@@ -17,26 +19,57 @@ public class Java_Application {
             URI uri=new URI("https://www.youtube.com");
         } catch (URISyntaxException e) {
             e.printStackTrace();
+
+            System.out.println(e.getMessage());
         }
 
-        int[] d;
+        HashMap<Integer, Integer> hashMap= new HashMap<>();
 
-        d=new int[]{1,2,3};
+        hashMap.put(0,1);
+        hashMap.put(1,2);
 
-        String  user_string = getInput("");
+        System.out.println(hashMap);
+
+        Set<Integer> keys=hashMap.keySet();
+
+        Iterator<Integer> iterator = keys.iterator();
+
+        while(iterator.hasNext()){
+            Integer value=iterator.next();
+            System.out.print(value+hashMap.get(value));
+        }
+
 
      //   int average=  average(d);
 
-        char [] ch = user_string.toCharArray();
+  /*      char [] ch = user_string.toCharArray();
+                       int[] d;
 
+          d=new int[]{1,2,3};
     //    System.out.println("Hello_World"+user_string+average);
 
           user_string.trim();
+
           
         for(char c:ch){
 
             System.out.println(c);
         }
+
+          ArrayList<String>arrayList=new ArrayList<String>();
+
+
+
+        String  user_string = getInput("");
+
+
+          while(user_string.length()!=0)
+            arrayList.add(user_string);
+
+        System.out.println(arrayList);
+
+
+        */
 
     }
 
